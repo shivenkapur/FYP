4efcf46d705b4aa212be0d14430de75a27f40997
Shiven Kapur <shiven@hku.hk>
2020-04-19 17:10:34 +0800

    Framework added

--- GoogleSearch/test.js
+++ GoogleSearch/test.js
@@ -3,10 +3,10 @@ import api from './api.js'
 
 export default async function start(searchKeywords){
 
-    let data = await api.getAllLinks('node.js best practices');
+    let data = await api.getAllLinks('Why is coronavirus so stupid?');
     console.log(data);
 
-    data = await api.getAllMetaData('node.js worst practices');
+    data = await api.getAllMetaData('Help me corona');
     console.log(data);
     
 }
