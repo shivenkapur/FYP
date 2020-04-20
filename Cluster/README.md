baa0cb4a913290e9a603b13a1c894c2fe505d0ea
Shiven Kapur <shiven@hku.hk>
2020-04-20 11:56:44 +0800

    servers added to 3 modules: not tested

---
+++ Cluster/README.md
@@ -0,0 +1,2 @@
+#For cluster analysis and other graph based data science tools
+https://neo4j.com/graph-data-science-library/
---
+++ GoogleSearch/server.js
@@ -0,0 +1,9 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.use('/getAllLinks', getAllLinks);
+app.use('/getAllMetaData', getAllMetaData);
+app.get('/', (req, res) => res.send('Hello World!'))
+
+app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
