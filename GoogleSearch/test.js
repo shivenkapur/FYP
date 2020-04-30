35158350d2cc678927afa9ac7dc343724b9375e1
Shiven Kapur <shiven@hku.hk>
2020-04-30 19:28:31 +0800

    frontend framework added

--- GoogleSearch/test.js
+++ GoogleSearch/test.js
@@ -8,10 +8,8 @@ export default async function start(searchKeywords){
     });
     
     let data = await api.getAllLinks('Why is coronavirus so stupid?');
-    console.log(data);
 
     data = await api.getAllMetaData('Help me corona');
-    console.log(data);
     
 }
 
--- Web Crawler/webCrawlingSpider.py
+++ Web Crawler/webCrawlingSpider.py
@@ -3,73 +3,73 @@ import scrapy
 from scrapy.exceptions import CloseSpider
 from scrapy.crawler import CrawlerRunner
 from bs4 import BeautifulSoup
-import requests 
-import threading 
+import requests
+import threading
 from twisted.internet import reactor
 
+
 class WebCrawler(scrapy.Spider):
-	
-	name = 'webCrawler'
-	start_urls = []
-	allowed_domains = []
-	number_of_pages_scraped = 0
 
-	def parse(self, response):
+    name = 'webCrawler'
+    start_urls = []
+    allowed_domains = []
+    number_of_pages_scraped = 0
 
-		print("Link::::::::::::" + start_urls[0])
-		
-		if(response.status == 200):
-	 		# Extract the hrefs from initial google results
-			soup = BeautifulSoup(response.text, 'html.parser')
-			
-			# Feed them into the URL Queue
-			hyperlinks = []
+    def parse(self, response):
 
-			for link in soup.find_all('a'):
-				if(link.get('href') is not None):
-					if(link.get('href').startswith('http')):
-						hyperlinks.append(link.get('href'))
+        if(response.status == 200):
+            # Extract the hrefs from initial google results
+            soup = BeautifulSoup(response.text, 'html.parser')
 
-			#print(hyperlinks)
+            # Feed them into the URL Queue
+            hyperlinks = []
 
-			# kill all script and style elements
-			for script in soup(["script", "style"]):
-				script.decompose()    # rip it out
+            for link in soup.find_all('a'):
+                if(link.get('href') is not None):
+                    if(link.get('href').startswith('http')):
+                        hyperlinks.append(link.get('href'))
 
-			# Extract the normal text and put it into the Document Storage database
-			pageText = soup.get_text()
+            # print(hyperlinks)
 
-			# Publish extracted text
+            # kill all script and style elements
+            for script in soup(["script", "style"]):
+                script.decompose()    # rip it out
 
-			print(pageText)
+            # Extract the normal text and put it into the Document Storage database
+            pageText = soup.get_text()
 
-			if(self.number_of_pages_scraped > 100):		# Stopping condition
-				raise CloseSpider('Sufficient pages scraped')
+            # Publish extracted text
 
-			# Creation of new spiders for a url from URL queue through script
-			for url in hyperlinks:
-				self.number_of_pages_scraped += 1
-				yield scrapy.Request(url, self.parse)
+            print(pageText)
+
+            if(self.number_of_pages_scraped > 100):		# Stopping condition
+                raise CloseSpider('Sufficient pages scraped')
+
+            # Creation of new spiders for a url from URL queue through script
+            for url in hyperlinks:
+                self.number_of_pages_scraped += 1
+                yield scrapy.Request(url, self.parse)
+
 
 def main(message):
 
-	# Running WebCrawler spider from the script instead of the terminal
-	response = requests.get('http://www.google.com/search?q=amazon+strengths/')
+    # Running WebCrawler spider from the script instead of the terminal
+    response = requests.get('http://www.google.com/search?q=amazon+strengths/')
 
-	soup = BeautifulSoup(response.text, 'html.parser')
-	initialGoogleLinks = []
+    soup = BeautifulSoup(response.text, 'html.parser')
+    initialGoogleLinks = []
 
-	for link in soup.find_all('a'):
-		temp = link.get('href')
-		if(temp.startswith('/url?q=')):
-			stop = temp.find('&')					# Extracting appropriate URLs
-			initialGoogleLinks.append(temp[7:stop])
-	
-	runner = CrawlerRunner()
+    for link in soup.find_all('a'):
+        temp = link.get('href')
+        if(temp.startswith('/url?q=')):
+            stop = temp.find('&')					# Extracting appropriate URLs
+            initialGoogleLinks.append(temp[7:stop])
 
-	for link in initialGoogleLinks:
-		runner.crawl(WebCrawler, start_urls = [link])
+    runner = CrawlerRunner()
 
-	d = runner.join()
-	d.addBoth(lambda _: reactor.stop())
-	reactor.run() 
+    for link in initialGoogleLinks:
+        runner.crawl(WebCrawler, start_urls=[link])
+
+    d = runner.join()
+    d.addBoth(lambda _: reactor.stop())
+    reactor.run()
